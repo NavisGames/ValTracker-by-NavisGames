34b7a920d2d1735ffadd087df9ae8583e2dde118
NavisGames <lakendianer@gmail.com>
2024-06-22 18:15:55 +0200

    Changed Title to MAJOR.MINOR instead of MAJOR.MINOR.PATCH

--- ValorantTracking/ValorantTracking.py
+++ ValorantTracking/ValorantTracking.py
@@ -58,7 +58,7 @@ class Ui_ValorantTrackerByNavisGames(object):
             valorant_tracking_by_navisgames.setFont(font)
             valorant_tracking_by_navisgames.setMouseTracking(False)
             valorant_tracking_by_navisgames.setWindowTitle(
-                "Valorant Tracking 2.4.7 By NavisGames"
+                "Valorant Tracking 2.4 By NavisGames"
             )
             icon = QtGui.QIcon()
             iconImage = Path(__file__).parent.joinpath("Images\\icon.png")
